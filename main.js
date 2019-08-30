1f5187e915211c52c3adfc5cd15f9d4bf7b2712c
Gluten Free <camiloa0318@gmail.com>
2019-08-29 19:41:57 -0500

    Add fahrenheit converter

--- main.js
+++ main.js
@@ -2,18 +2,31 @@ let celciusIn = document.querySelector('#celcius > input');
 let fahrenheitIn = document.querySelector('#fahrenheit > input');
 let kelvinIn = document.querySelector('#kelvin > input');
 
-celciusIn.addEventListener('input', converter);
+celciusIn.addEventListener('input', celciusConverter);
+fahrenheitIn.addEventListener('input', fahConverter);
+kelvinIn.addEventListener('input', kelConverter);
 
 
 
 
 
 
-
-function converter(){
+function celciusConverter(){
 	let cTemp = parseFloat(celciusIn.value);
 	let fTemp = (cTemp * (9/5)) + 32;
 	let kTemp = cTemp + 273.15;
 	fahrenheitIn.value = fTemp;
 	kelvinIn.value = kTemp;
+}
+function fahConverter(){
+	let fTemp = parseFloat(fahrenheitIn.value);
+	let cTemp = (fTemp - 32) * (5/9);
+	let kTemp = fTemp / 273.15;
+	celciusIn.value = cTemp;
+	kelvinIn.value = kTemp;
+}
+
+
+function kelConverter(){
+	
 }
